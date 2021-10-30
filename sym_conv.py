486e0fc258e16b431f936b929dedabbbe4f240a4
tnakaicode <tnakaicode@gmail.com>
2021-10-31 04:35:20 +0900

    format

--- sym_conv.py
+++ sym_conv.py
@@ -1,34 +1,45 @@
-import ast, inspect
+import ast
+import inspect
 import numpy as np
 import sympy as sp
+from types import FunctionType
+
 
 def f(a):
     return np.array([np.sin(a), np.cos(a)])
 
+
 def f2(a):
     return np.array([1, np.sin(a)])
+
 
 def f3(a):
     return f(a) + f2(a)
 
-translate = {'array': 'Array'}
 
 class np_to_sp(ast.NodeTransformer):
+    translate = {'array': 'Array'}
+
     def visit_Name(self, node):
-        if node.id=='np':
+        if node.id == 'np':
             node = ast.copy_location(ast.Name(id='sp', ctx=node.ctx), node)
         return node
+
     def visit_Attribute(self, node):
         self.generic_visit(node)
-        if node.value.id=='sp' and node.attr in translate:
+        if node.value.id == 'sp' and node.attr in self.translate:
             fields = {k: getattr(node, k) for k in node._fields}
-            fields['attr'] = translate[node.attr]
+            fields['attr'] = self.translate[node.attr]
             node = ast.copy_location(ast.Attribute(**fields), node)
         return node
 
-from types import FunctionType
+
+z = ast.parse(inspect.getsource(f))
+np_to_sp().visit(z)
+#exec(compile(z, '', 'exec'))
 
 for fn in f3.__code__.co_names:
+    print(fn)
     fo = globals()[fn]
     if not isinstance(fo, FunctionType):
         continue
